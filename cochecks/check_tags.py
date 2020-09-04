4a3d8fc1993c2a1499a1b3e2602b8d22905dd95c
yikeke <yikeke@pingcap.com>
2020-09-04 18:29:14 +0800

    first release: 0.0.1

---
+++ cochecks/check_tags.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+def tag_():
+    print('your tags are checked!')
+
+def filename():
+    print('your filenames are checked!')
+
+def imagename():
+    print('your imagenames are checked!')
---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pingcap-docs-checks',
+    version='0.0.1',
+    author='yikeke',
+    author_email='yikeke@pingcap.com',
+    url='https://github.com/yikeke/pingcap-docs-checks',
+    description='Provides scripts to check markdown files from pingcap docs repositories',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    keywords='check-unclosed-tags file-processing markdown docs-like-code',
+    install_requires=[],
+    # folder/file names can not contain "-"
+    entry_points={
+        'console_scripts': [
+            'tag=cochecks.check_tags:tag',
+            'file=cochecks.check_tags:filename',
+            'image=cochecks.check_tags:imagename'
+        ],
+    }
+)
