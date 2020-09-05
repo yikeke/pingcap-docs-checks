03233048e6ad819e08551e64d03afa1efc5841de
yikeke <yikeke@pingcap.com>
2020-09-05 14:42:59 +0800

    release 0.0.2

---
+++ build/lib/cochecks/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# encoding=utf-8
+
+def tag():
+    print('your tags are checked!')
+
+def filename():
+    print('your filenames are checked!')
+
+def imagename():
+    print('your imagenames are checked!')
