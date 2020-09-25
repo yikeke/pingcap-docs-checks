ace5f5f26f229e77bc76ef29fc10a7d3ea12f20d
yikeke <yikeke@pingcap.com>
2020-09-25 11:16:16 +0800

    release 0.0.11

--- cochecks/__init__.py
+++ cochecks/__init__.py
@@ -204,7 +204,7 @@ def process(opt):
         # print(parser.print_help())
 
 def exe_main():
-    parser = OptionParser(version="%prog 0.0.9")
+    parser = OptionParser(version="%prog 0.0.11")
     parser.set_defaults(verbose=True)
     # parser.add_option("-a", "--all", dest="all",
     #                   help="Checks unclosed tags, code blocks, copyable snippets, etc.", metavar="ALL")
